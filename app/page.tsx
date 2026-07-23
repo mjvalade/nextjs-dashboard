2aea33a156909e9e9d7b1518dd376e926289d0c5
MaryJane Valade <mjvalade@gmail.com>
2026-07-23 14:37:06 -0600

    Finish dashboard setup

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
+            loading="eager"
           />
         </div>
       </div>
