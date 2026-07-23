5d7f285bb94ff5ca12600cc4306810eab87a7b2d
MaryJane Valade <mjvalade@gmail.com>
2026-07-23 14:30:32 -0600

    Set up fonts, HP images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Points to the parent directory containing node_modules/next
+    root: path.join(__dirname, '../../'),
+  },
 };
 
 export default nextConfig;
